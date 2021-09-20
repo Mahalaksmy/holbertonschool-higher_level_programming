01267e06585ca010cd6ea9b5c7e0c2699043bfc2
Mahalaksmy <3354@holbertonschool.com>
2021-09-20 14:20:25 -0500

    Task add 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,6 @@ def roman_to_int(roman_string):
     num_romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if type(roman_string) is not str or roman_string == 0:
         return 0
-
     for i in roman_string:
         if i in roman_string:
             nget = num_romans.get(i)
