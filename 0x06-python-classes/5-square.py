f34b26af4f3be194f06848089081845a1f7b58fb
Mahalaksmy <3354@holbertonschool.com>
2021-09-28 20:59:19 -0500

    Task 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,38 +3,37 @@
 
 
 class Square:
-    """ Aclass Square that defines a square by: (based on 0-square.py)"""
-    def __init__(self, size=0):
-        """Initialize square with the size""""
-        self.__size = size
+	""" Aclass Square that defines a square by: (based on 0-square.py)"""
+	def __init__(self, size=0):
+		"""Initialize square with the size"""
+		self.__size = size
 
-    def area(self):
-        """Public instance method that returns the current square area"""
-        return self.__size * self.__size
+	def area(self):
+		"""Public instance method that returns the current square area"""
+		return self.__size * self.__size
 
-    @property
-    def size(self):
-        """Method to retrieve"""
-        return self.__size
+	@property
+	def size(self):
+		"""Method to retrieve"""
+		return self.__size
 
-    @size.setter
-    def size(self, value):
-        """Setter method"""
-        self.__size = value
+	@size.setter
+	def size(self, value):
+		"""Setter method"""
+		self.__size = value
 
-		""" Exception with the message"""
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
+		if type(value) is not int:
+			raise TypeError("size must be an integer")
 
-        if value < 0:
-            raise ValueError("size must be >= 0")
+		if value < 0:
+			raise ValueError("size must be >= 0")
 
-    def my_print(self):
-        """Method that prints the square"""
-        if self.__size > 0:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end="")
-                print("")
-        else:
-            print("")
+	def my_print(self):
+		"""Method that prints the square"""
+		if self.__size > 0:
+			for i in range(self.__size):
+				for j in range(self.__size):
+					print("#", end="")
+				print("")
+		else:
+			print("")
