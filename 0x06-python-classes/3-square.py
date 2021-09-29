834e8bd2d980193b107885d1fd50f6fb124bb44d
Mahalaksmy <3354@holbertonschool.com>
2021-09-28 19:28:24 -0500

    Task 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""New Class Square"""
+
+
+class Square:
+    """ Aclass Square that defines a square by: (based on 0-square.py)"""
+    def __init__(self, size=0):
+        """Initialize square with the size"""
+        self.__size = size
+
+        """ Exception with the message"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return self.__size *self.__size
