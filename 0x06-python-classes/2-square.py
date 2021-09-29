667cfccdcda50b758f27dcf71f62722b8978036e
Mahalaksmy <3354@holbertonschool.com>
2021-09-28 19:18:48 -0500

    Task 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""New Class Square"""
+
+
+class Square:
+    """ Aclass Square that defines a square by: (based on 0-square.py)"""
+    def __init__(self, size=0):
+        """Initialize square with the size"""
+        self.__size = size
+
+        """ Exception with the message"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
