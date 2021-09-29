ca83dae8a103f40ca57fc7afb079434420bd3e7f
Mahalaksmy <3354@holbertonschool.com>
2021-09-29 16:05:55 -0500

    Task 1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""New class rectangle """
+
+
+class Rectangle:
+    """New class rectangle that defines a rectangle:"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize square with the size width and height"""
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """property  to retrieve it"""
+        return self.width
+
+    @width.setter
+    def width(self, value):
+        """New value for width"""
+
+        """ Exception with the message"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__widget = value
+
+    @property
+    def height(self):
+        """property  to retrieve it"""
+        return self.height
+
+    @height.setter
+    def height(self, value):
+        """New value for width"""
+
+        """ Exception with the message"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__height = value
