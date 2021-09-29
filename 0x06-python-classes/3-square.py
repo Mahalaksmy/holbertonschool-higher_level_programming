a23dfc219d33ca190d43ee8f63794a2293337ceb
Mahalaksmy <3354@holbertonschool.com>
2021-09-28 19:37:27 -0500

    Task 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,4 +17,4 @@ class Square:
     
     def area(self):
         """Public instance method that returns the current square area"""
-        return self.__size *self.__size
+        return self.__size * self.__size
