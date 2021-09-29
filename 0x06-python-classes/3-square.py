d87c45abef922a571a5cc57cafd0db741ae86867
Mahalaksmy <3354@holbertonschool.com>
2021-09-28 19:39:24 -0500

    Task 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -14,7 +14,7 @@ class Square:
 
         if size < 0:
             raise ValueError("size must be >= 0")
-    
+
     def area(self):
         """Public instance method that returns the current square area"""
         return self.__size * self.__size
