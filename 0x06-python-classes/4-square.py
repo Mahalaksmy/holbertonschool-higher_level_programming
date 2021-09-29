19f2d102ae4ac32e3b1c720b282d05f22a213c2d
Mahalaksmy <3354@holbertonschool.com>
2021-09-28 22:21:27 -0500

    Update Task 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,26 +4,27 @@
 
 class Square:
     """ Aclass Square that defines a square by: (based on 0-square.py)"""
-    def _init_(self, size=0):
+    def __init__(self, size=0):
         """Initialize square with the size"""
         self.__size = size
 
-        def area(self):
-            """Public instance method that returns the current square area"""
-            return self._size * self._size
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return self.__size * self.__size
 
-        @property
-        def size(self):
-            """Method to retrieve"""
-            return self.__size
+    @property
+    def size(self):
+        """Method to retrieve"""
+        return self.__size
 
-        @size.setter
-        def size(self, value):
-            """Setter method"""
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        """Setter method"""
+        self.__size = value
 
-            """ Exception with the message"""
-            if type(value) is not int:
-                raise TypeError("size must be an integer")
-            if value < 0:
-                raise ValueError("size must be >= 0")
+        """ Exception with the message"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
