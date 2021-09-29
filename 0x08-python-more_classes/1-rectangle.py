90e4c1f0d98ad6545130e47595907cc62636eaa2
Mahalaksmy <3354@holbertonschool.com>
2021-09-29 16:26:53 -0500

    Task 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,25 +7,26 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize square with the size width and height"""
+
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """property  to retrieve it"""
+        """New value for width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """New value for width"""
+        """Setting a new value for width"""
 
         if type(value) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError("width must be an integer")
 
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("width must be >= 0")
 
-        self.__widget = value
+        self.__width = value
 
     @property
     def height(self):
@@ -34,12 +35,12 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """New value for width"""
+        """ New value for height"""
 
         if type(value) is not int:
-            raise TypeError("size must be an integer")
+            raise TypeError("height must be an integer")
 
         if value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError("height must be >= 0")
 
         self.__height = value
