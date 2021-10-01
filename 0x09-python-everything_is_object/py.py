d43c14276c452f269fb5d0581ede7c599e998cc3
Mahalaksmy <3354@holbertonschool.com>
2021-09-30 20:16:47 -0500

    Project 0x09-python-everything_is_object

---
+++ 0x09-python-everything_is_object/py.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+s1 = "Best School"
+s2 = s1
+print(s1 == s2)
+
+s1 = "Best"
+s2 = s1
+print(s1 is s2)
+
+s1 = "Best School"
+s2 = "Best School"
+print(s1 == s2)
+
+s1 = "Best School"
+s2 = "Best School"
+print(s1 is s2)
+
+l1 = [1, 2, 3]
+l2 = [1, 2, 3] 
+print(l1 == l2)
+
+l1 = [1, 2, 3]
+l2 = [1, 2, 3] 
+print(l1 is l2)
+
+l1 = [1, 2, 3]
+l2 = l1
+print(l1 == l2)
+
+l1 = [1, 2, 3]
+l2 = l1
+print(l1 is l2)
+
+l1 = [1, 2, 3]
+l2 = l1
+l1.append(4)
+print(l2)
+
+l1 = [1, 2, 3]
+l2 = l1
+l1 = l1 + [4]
+print(l2)
+
+def increment(n):
+    n += 1
+
+a = 1
+increment(a)
+print(a)
+
+
+def increment(n):
+    n.append(4)
+
+l = [1, 2, 3]
+increment(l)
+print(l)
+
+def assign_value(n, v):
+    n = v
+
+l1 = [1, 2, 3]
+l2 = [4, 5, 6]
+assign_value(l1, l2)
+print(l1)
