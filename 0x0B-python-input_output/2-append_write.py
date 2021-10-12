68ffe6eaf3ade7a40529e0eb698d7330ae98be51
Mahalaksmy <3354@holbertonschool.com>
2021-10-12 18:16:24 -0500

    Update Task 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,10 +5,10 @@ at the end of a text file (UTF8)
 and returns the number of characters added:"""
 
 
-def write_file(filename="", text=""):
-    """This is a function that appends
+def append_write(filename="", text=""):
+    """"This is a function that appends
     at the end of a text file"""
 
     with open(filename, 'a', encoding="utf8") as filen:
         f = filen.write(text)
-        return f
+    return f
