c88adefbee34ae7805ae5c09e2ba7d3dd42d5ca9
Mahalaksmy <3354@holbertonschool.com>
2021-10-12 17:07:11 -0500

    Project 0x0B. Python - Input/Output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Creation of the function 
+that reads a text file and prints it to stdout."""
+
+
+def read_file(filename=""):
+    """This is a function that reads
+    and prints files"""
+    
+    with open(filename, encoding="utf8") as filen:
+        for line in filen:
+            print(line, end="")
