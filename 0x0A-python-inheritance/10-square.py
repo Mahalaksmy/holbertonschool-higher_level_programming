0e9e1dca8b8706d3ea58a2a1348c97e36ae05b97
Mahalaksmy <3354@holbertonschool.com>
2021-10-11 19:15:48 -0500

    update Task 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" a class Square that inherits
+from Rectangle (9-rectangle.py):"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+        """Instantiation with size"""
+
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Area of rhe Square"""
+
+        return self.__size ** 2
