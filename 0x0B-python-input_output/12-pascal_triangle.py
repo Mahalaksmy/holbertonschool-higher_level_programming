7353435b7290a600e64ef059ca4b3ca3c3dc3466
Mahalaksmy <3354@holbertonschool.com>
2021-10-12 22:06:02 -0500

    12-pascal_triangle.py

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Creation a function that
+returns a list of list of integers
+the Pascal’s triangle of n"""
+
+
+def pascal_triangle(n):
+    """Function the Pascal’s triangle of n"""
+
+    if n <= 0:
+        return []
+
+    pascal_triangle = [[] for i in range(n)]
+    for i in range(n):
+        for j in range(i+1):
+            if(j < i):
+                if(j == 0):
+                    pascal_triangle[i].append(1)
+                else:
+                    pascal_triangle[i].append(pascal_triangle[i-1][j]
+                                              + pascal_triangle[i-1][j-1])
+            elif(j == i):
+                pascal_triangle[i].append(1)
+    return pascal_triangle
