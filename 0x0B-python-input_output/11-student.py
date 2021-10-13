57eb7a14ccefdb552a18b794af7ee6401f7c2282
Mahalaksmy <3354@holbertonschool.com>
2021-10-12 20:54:40 -0500

    Task 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Creation of the
+a class Student that defines a student """
+
+
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        """Instantion of the class"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Method thar retrieves a dictionary """
+
+        if attrs is not None:
+            listx = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    listx[i] = self.__dict__[i]
+            return listx
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Method that replaces all atributes
+        of the students"""
+
+        for x in json:
+            self.__dict__.update({x: json[x]})
