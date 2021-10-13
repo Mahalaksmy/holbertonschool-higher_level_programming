21476c33e69010b98025dd540f15489eb591aff4
Mahalaksmy <3354@holbertonschool.com>
2021-10-12 19:57:55 -0500

    Task 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Creation a function that
+returns the dictionary
+description with simple data structure
+for Json an object”"""
+
+
+
+def class_to_json(obj):
+    """ This a function  that returns
+    the dictionary for Json"""
+
+    return obj.__dict__
