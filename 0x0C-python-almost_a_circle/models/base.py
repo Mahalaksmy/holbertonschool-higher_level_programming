a59bf235abe95b6f4bffd3be62356dec50ce7471
Mahalaksmy <3354@holbertonschool.com>
2021-10-18 11:27:24 -0500

    0x0C. Python - Almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This class will be the base
+of all other classes in this project"""
+
+import os.path
+
+class Base:
+    """Class Base"""
+    
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """Initialize a Base instance"""
+        
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
