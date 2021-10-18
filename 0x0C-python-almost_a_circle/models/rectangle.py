f8de8d2fc44d767633be8b05717bfcdbd288b7f6
Mahalaksmy <3354@holbertonschool.com>
2021-10-18 16:47:26 -0500

    Task __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,3 +88,13 @@ class Rectangle(Base):
             for j in range(self.__width):
                 print("#", end="")
             print('')
+    
+    def __str__(self):
+        """ This a Method that returns
+        [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.__x,
+                                                       self.__y,
+                                                       self.__width,
+                                                       self.__height)
