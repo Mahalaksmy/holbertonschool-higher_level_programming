65e2ce526108d39e341122f31b1b58796bbf8203
Mahalaksmy <3354@holbertonschool.com>
2021-10-18 17:24:29 -0500

    Update Task Project

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,3 +98,19 @@ class Rectangle(Base):
                                                        self.__y,
                                                        self.__width,
                                                        self.__height)
+    
+    def update(self, *args):
+        """This method assigns an argument
+        to each attribute"""
+        
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
