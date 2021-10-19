1b9a4b771cba73e6c281f6b04049f8aa7ba3b102
Mahalaksmy <3354@holbertonschool.com>
2021-10-18 20:53:19 -0500

    Task 12.Square update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -36,6 +36,21 @@ class Square(Rectangle):
             raise ValueError('width must be > 0')
         self.width = value
         self.height = value
+    
+    def update(self, *args, **kwargs):
+        """Update the square: id, size, x, y."""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
         
         
         
