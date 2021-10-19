ac03932dbe2afc1166fe979481bd9c5751df4dbb
Mahalaksmy <3354@holbertonschool.com>
2021-10-19 10:38:58 -0500

    Update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,33 +2,34 @@
 """The class Square
 that inherits from Rectangle"""
 
-from models.rectangle import  Rectangle
+from models.rectangle import Rectangle
+
 
 class Square(Rectangle):
     """Class Square that inherits
     from Rectangle"""
-    
+
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize Square Class"""
         super().__init__(size, size, x, y, id)
         self.size = size
-        
+
     def __str__(self):
         """ This a Method that returns
         [Square] (<id>) <x>/<y> - <size>"""
-        
+
         return "[Square] ({}) {}/{} - {}".format(self.id,
-                                                       self.x,
-                                                       self.y,
-                                                       self.width)
+                                                 self.x,
+                                                 self.y,
+                                                 self.width)
 
     @property
     def size(self):
         """getter of the self"""
         return self.width
-    
+
     @size.setter
-    def size(self,value):
+    def size(self, value):
         """Setter of the size"""
         if type(value) is not int:
             raise TypeError('width must be an integer')
@@ -36,7 +37,7 @@ class Square(Rectangle):
             raise ValueError('width must be > 0')
         self.width = value
         self.height = value
-    
+
     def update(self, *args, **kwargs):
         """Update the square: id, size, x, y."""
         if args:
@@ -55,12 +56,8 @@ class Square(Rectangle):
     def to_dictionary(self):
         """This a Method that returns
         the dictionary representation"""
-            
+
         return {'id': self.id,
-                'size' : self.width,
-                'x' : self.x,
-                'y' : self.y}
-        
-        
-        
-        
+                'size': self.width,
+                'x': self.x,
+                'y': self.y}
