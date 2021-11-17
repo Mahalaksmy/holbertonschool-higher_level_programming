94c43aafa8dfce536614aed251f6d2ddee55f9ce
Mahalaksmy <3354@holbertonschool.com>
2021-11-17 14:56:54 -0500

    Cities California

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Cities of California
 -- This is a script that lists all the cities that can be found in the database
-SELECT id, name FROM cities WHERE name = 'California' ORDER by id ASC;
+SELECT id, name FROM cities WHERE id = 1 ORDER by id ASC;
