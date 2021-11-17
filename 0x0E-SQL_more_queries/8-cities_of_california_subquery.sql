6899bd29b38545402e1bbcd89f104a4085f42fe9
Mahalaksmy <3354@holbertonschool.com>
2021-11-17 16:59:37 -0500

    Update Tasks

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Cities of California
 -- This is a script that lists all the cities that can be found in the database
-SELECT state_id, name FROM cities WHERE state_id = 1 ORDER by id ASC;
+SELECT id, name FROM cities WHERE state_id = 1 ORDER by id ASC;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Cities by States
 -- This is a script that list all cities contained in the database
 SELECT cities.id, cities.name, states.nam FROM cities
-INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id DESC;
+INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
