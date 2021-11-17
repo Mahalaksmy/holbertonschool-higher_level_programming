3478764b08052f3876e2c5ec99458c62b6c400cc
Mahalaksmy <3354@holbertonschool.com>
2021-11-17 18:01:13 -0500

    Update Task Genre ID by show

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,3 @@
 -- Cities by States
 -- This is a script that list all cities contained in the database
-SELECT cities.id, cities.name, states.nam FROM cities
-INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.nam FROM cities INNER JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC;
