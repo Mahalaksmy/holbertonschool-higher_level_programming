8b22663b14242fc42b45233a96e456dc34f5b15a
Mahalaksmy <3354@holbertonschool.com>
2021-11-17 21:05:21 -0500

    Number of shows by genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- Number of shows by genre
+-- script that lists all genres and displays the number.
+SELECT tv_genres.name AS genre,
+COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres JOIN tv_show_genres ON tv_genres.id =
+    tv_show_genres.genre_id GROUP BY 1 ORDER BY 2 DESC;
