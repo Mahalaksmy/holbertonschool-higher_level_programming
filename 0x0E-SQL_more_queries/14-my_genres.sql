1beb72132fea90137df3f65f5421a9d3f1a7afa2
Mahalaksmy <3354@holbertonschool.com>
2021-11-17 21:43:28 -0500

    Task 14. My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,3 @@
---My genres
+-- My genres
 --  script that uses the database to lists all genres.
-SELECT tv_genres.name FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name ASC;
+SELECT tv_genres.name FROM tv_shows INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_shows.title = 'Dexter' ORDER BY tv_genres.name ASC;
