a55ce720235f627b8c97e027e79b468ecd6f44d2
Mahalaksmy <3354@holbertonschool.com>
2021-12-15 10:10:40 +0100

    Task 3

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* This is a a script that prints the first argument passed to it */
+const myArgs = process.argv[2];
+if (myArgs !== undefined) {
+  console.log(myArgs);
+} else {
+  console.log('No Argument');
+}
