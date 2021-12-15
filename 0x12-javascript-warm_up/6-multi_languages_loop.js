149a7cc1b55a5d6ba67caa6ac32c47471e818494
Mahalaksmy <3354@holbertonschool.com>
2021-12-15 11:15:16 +0100

    Task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/* This a a script that prints 3 lines but using an array */
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (let i = 0; lang[i]; i++) {
+  console.log(lang[i]);
+}
