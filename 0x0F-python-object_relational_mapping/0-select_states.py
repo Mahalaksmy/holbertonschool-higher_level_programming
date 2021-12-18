f6e11fbe6e00f1b9711185a43a2c20beeac58aca
Mahalaksmy <3354@holbertonschool.com>
2021-12-18 17:28:29 +0100

    Update query

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -17,10 +17,14 @@ if __name__ == '__main__':
                            db=database_name,
                            charset="utf8")
     cur = conn.cursor()
+
     # HERE I have to know SQL to grab all states in my database
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+
     query_rows = cur.fetchall()
+
     for row in query_rows:
         print(row)
+
     cur.close()
     conn.close()
