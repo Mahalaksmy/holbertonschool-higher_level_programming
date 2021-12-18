e6274d509c8ec41fbc515e61ae7df8c5e264f21f
Mahalaksmy <3354@holbertonschool.com>
2021-12-18 16:49:03 +0100

    0x0F. Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This is a  a script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                        passwd=mysql_password, db=database_name, charset="utf8")
+    cur = conn.cursor()
+    # HERE I have to know SQL to grab all states in my database
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
