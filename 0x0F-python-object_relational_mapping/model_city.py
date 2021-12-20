45c7daf538972eda870f329df0e0980394299d65
= <=>
2021-12-20 13:09:18 -0800

    Files Task 14

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This is a file  that contains the class definition of a City.
+"""
+from model_state import Base, State
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
+
+class City(Base):
+
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+
+    name = Column(String(128), nullable=False)
+
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
