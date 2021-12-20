cc60f1a23147b1c155c41cc40643473d93e14047
= <=>
2021-12-20 07:37:03 -0800

    Task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,23 +5,21 @@ from the database hbtn_0e_6_usa
 """
 
 import sys
-import sqlalchemy as create_engine
 from model_state import Base, State
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
 
-    """ Conecting to a database """
+if __name__ == "__main__":
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    md = create_engine.MetaData()
     Session = sessionmaker(bind=engine)
-
     session = Session()
-
-    for instance in session.query(State).order_by(State.id).all():
-        print("{}: {}".format(instance.id, instance.name))
+    for state in session.query(State).order_by(State.id).all():
+        print('{}: {}'.format(state.id, state.name))
 
     session.close()
