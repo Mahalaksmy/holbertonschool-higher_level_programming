77373d44f5bfb6a9a0b4ccb5c69f7e943f072f98
= <=>
2021-12-20 07:50:25 -0800

    task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,16 +13,12 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
-    md = create_engine.MetaData()
     Session = sessionmaker(bind=engine)
-
     session = Session()
-    instance = session.query(State).first()
-
-    if instance is not None:
-        print('{}: {}'.format(instance.id, instance.name))
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print('{}: {}'.format(state.id, state.name))
     else:
         print('Nothing')
-
-    session.close()
