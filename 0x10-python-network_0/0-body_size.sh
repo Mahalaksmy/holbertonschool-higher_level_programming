0750dad778e5d36e4308ff92af6fcc67eb05e6f1
Mahalaksmy <3354@holbertonschool.com>
2022-01-08 22:25:40 +0100

    Update Task 0-body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This is a Script bash that takes in a URL, sends a request to that URL
-curl -s "$1" | grep -e "Content-Length" | wc -c
+curl -sI $1 | grep -e "Content-Length" | cut -d" " -f2
