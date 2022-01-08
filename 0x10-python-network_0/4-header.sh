43f51465e61933490bfe3b405dc1f470753dedb9
Mahalaksmy <3354@holbertonschool.com>
2022-01-08 23:47:05 +0100

    Task 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This is a Script bash that takes in a URL and displays all HTTP methods the server will accept
+# This is a Script bash that takes in a URL and displays.
 curl -sX GET -H "X-HolbertonSchool-User-Id: 98" "$1"
