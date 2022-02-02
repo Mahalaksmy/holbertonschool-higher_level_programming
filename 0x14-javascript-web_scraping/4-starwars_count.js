d8c563bc453a8bd30703b775257bc4db5e4dc249
Mahalaksmy <3354@holbertonschool.com>
2022-02-02 15:42:45 +0100

    Task 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,19 +3,19 @@ const request = require("request");
 const url = process.argv[2];
 
 request(url, (err, response, body) => {
-    if (err) {
-        console.log(err);
-    } else {
-        const data = JSON.parse(body);
-        let i = 0;
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body);
+    let i = 0;
 
-        for (const movie of data.results) {
-            for (const char of movie.characters) {
-                if (char.endsWith('18/')) {
-                    i++;
-                }
-            }
+    for (const movie of data.results) {
+      for (const char of movie.characters) {
+        if (char.endsWith('18/')) {
+          i++;
         }
-        console.log(i);
+      }
     }
+    console.log(i);
+  }
 });
