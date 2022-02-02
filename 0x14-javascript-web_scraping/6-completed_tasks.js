0188d40ea61e2a6bf2d960f147255b9642ace083
Mahalaksmy <3354@holbertonschool.com>
2022-02-02 16:47:30 +0100

    Update indentation

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,23 +3,23 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (err, resp, body) => {
-    if (err) {
-        console.log(err);
-    } else {
-        const data = {};
-        const todo = JSON.parse(body);
-        for (let i = 0; i < todo.length; i++) {
-            const userId = todo[i].userId;
-            const taskStatus = todo[i].completed;
+  if (err) {
+    console.log(err);
+  } else {
+    const data = {};
+    const todo = JSON.parse(body);
+    for (let i = 0; i < todo.length; i++) {
+      const userId = todo[i].userId;
+      const taskStatus = todo[i].completed;
 
-            if (taskStatus === true) {
-                if (!data[userId]) {
-                    data[userId] = 1;
-                } else {
-                    data[userId] += 1;
-                }
-            }
+      if (taskStatus === true) {
+        if (!data[userId]) {
+          data[userId] = 1;
+        } else {
+          data[userId] += 1;
         }
-        console.log(data);
+      }
     }
+    console.log(data);
+  }
 });
