e5364fb71652928357a69a0fc947e22336d08c23
Mahalaksmy <3354@holbertonschool.com>
2022-02-02 16:42:01 +0100

    Update Task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,22 +3,23 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, (err, resp, body) => {
-  if (err) throw err;
+    if (err) {
+        console.log(err);
+    } else {
+        const data = {};
+        const todo = JSON.parse(body);
+        for (let i = 0; i < todo.length; i++) {
+            const userId = todo[i].userId;
+            const taskStatus = todo[i].completed;
 
-  const data = {};
-  const todo = JSON.parse(body);
-
-  for (let i = 0; i < todo.length; i++) {
-    const user_Id = todo[i].user_Id;
-    const taskStatus = todo[i].completed;
-
-    if (taskStatus === true) {
-      if (!data[user_Id]) {
-        data[user_Id] = 1;
-      } else {
-        data[user_Id] += 1;
-      }
+            if (taskStatus === true) {
+                if (!data[userId]) {
+                    data[userId] = 1;
+                } else {
+                    data[userId] += 1;
+                }
+            }
+        }
+        console.log(data);
     }
-  }
-  console.log(data);
 });
